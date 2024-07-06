bc86eda76fc472b7565163a553a392127eeb0f46
HernánRG <hernanrg85@gmail.com>
2024-07-06 17:23:09 +0200

    añadido simple_screen

--- zoo_separ.py
+++ zoo_separ.py
@@ -1,30 +1,33 @@
 # Importar funciones del archivo zoo_funciones
 from zoo_funciones import *
+from simple_screen import Screen_manager
 
-def main():
-    # Diccionario de tipos de entrada
-    tipos_entrada = {
-        "BEBE": {"EDAD": 3, "PRECIO": 0, "CONTADOR": 0},
-        "NIÑO": {"EDAD": 13, "PRECIO": 14, "CONTADOR": 0},
-        "ADULTO": {"EDAD": 65, "PRECIO": 23, "CONTADOR": 0},
-        "JUBILADO": {"EDAD": float('inf'), "PRECIO": 18, "CONTADOR": 0}
-    }
+with Screen_manager() as sc:
 
-    # Cabecera del programa
-    print_cabecera()
+    def main():
+        # Diccionario de tipos de entrada
+        tipos_entrada = {
+            "BEBE": {"EDAD": 3, "PRECIO": 0, "CONTADOR": 0},
+            "NIÑO": {"EDAD": 13, "PRECIO": 14, "CONTADOR": 0},
+            "ADULTO": {"EDAD": 65, "PRECIO": 23, "CONTADOR": 0},
+            "JUBILADO": {"EDAD": float('inf'), "PRECIO": 18, "CONTADOR": 0}
+        }
 
-    # Inicio del bucle para pedir edades
-    while True:
-        edad = input("\nEdad (pulsa enter para finalizar): ")
-        edad_Valida, edad_int = verificar_edad(edad)
-        if edad_Valida is None: # entra aqui porque verificar_edad marca None si usuario pulsa enter ""
-            break
-        else:
-            tipos_entrada = procesar_edad(edad_Valida, edad_int, tipos_entrada)
+        # Cabecera del programa
+        print_cabecera()
 
-    # Mostrar la información final por pantalla
-    imprimir_ticket(tipos_entrada)
+        # Inicio del bucle para pedir edades
+        while True:
+            edad = input("\nEdad (pulsa enter para finalizar): ")
+            edad_Valida, edad_int = verificar_edad(edad)
+            if edad_Valida is None: # entra aqui porque verificar_edad marca None si usuario pulsa enter ""
+                break
+            else:
+                tipos_entrada = procesar_edad(edad_Valida, edad_int, tipos_entrada)
 
-# Por si se importa para no ejecutarlo
-if __name__ == "__main__":
-    main()
+        # Mostrar la información final por pantalla
+        imprimir_ticket(tipos_entrada)
+
+    # Por si se importa para no ejecutarlo
+    if __name__ == "__main__":
+        main()
